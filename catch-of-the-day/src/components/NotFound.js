10026a130684789d356b163d8f12fc6abc5fae3b
Sean Swanson <sean-swan@hotmail.com>
2018-03-22 13:42:09 -0700

    Added not found router component, updating storepicker to have working button on form submit.

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => {
+    return(
+        <div>Content not found, yo!</div>
+    )
+}
+
+export default NotFound;
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,14 +1,22 @@
 import React, { Component, Fragment } from 'react';
-import { render } from 'react-dom';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  goToStore = (e) => {
+    e.preventDefault();
+    //1. Stop form from submitting
+    console.log("Going to store");
+    //2. Get text from input
+    //3. Change the page to /store/whatever-they-entered
+  }
+
   render(){
     return (
       <Fragment>
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore} >
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store name" />
-        <button type="submit">Visit Store →</button>
+        <input type="text" required placeholder="Store name" defaultValue={getFunName()}/>
+        <button type="submit" >Visit Store →</button>
       </form>
       </Fragment>
       )
